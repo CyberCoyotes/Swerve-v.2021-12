c7778e2b1271b80ad77d8362d1bf88df6cb15d7c
Jestin VanScoyoc <coyotepod@gmail.com>
2021-12-15 09:10:07 -0500

    Adding a rudimentary command

---
+++ src/main/java/frc/robot/commands/DriveFWD.java
@@ -0,0 +1,37 @@
+// Drive foward in auton
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+import java.util.function.DoubleSupplier;
+
+import javax.print.event.PrintJobAdapter;
+
+public class DriveForward extends CommandBase {
+    pribate final DrivetrainSubsystem 
+    // Name of the command
+    public DriveForward() {
+
+    }
+    
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+    
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished () {
+        return false;
+    }
+}
